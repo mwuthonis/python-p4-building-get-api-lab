9dfb96536ceb6614a9cff690ef25cb9716f8e439
mwuthonis <muthonishelmith136@gmail.com>
2025-06-02 19:40:10 +0300

    Build a n API to handle GET request

--- server/app.py
+++ server/app.py
@@ -20,19 +20,28 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakeries_list = [bakery.to_dict(rules=('baked_goods',)) for bakery in bakeries]
+    return make_response(jsonify(bakeries_list), 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get_or_404(id)
+    return make_response(jsonify(bakery.to_dict(rules=('baked_goods',))), 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_list = [bg.to_dict(rules=('bakery',)) for bg in baked_goods]
+    return make_response(jsonify(baked_goods_list), 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if baked_good:
+        return make_response(jsonify(baked_good.to_dict(rules=('bakery',))), 200)
+    else:
+        return make_response(jsonify({}), 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
